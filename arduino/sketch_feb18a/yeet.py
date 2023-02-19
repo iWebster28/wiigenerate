6ea645c92ee3f3ec0341591a2160bdcf252904dd
David <david990724@gmail.com>
2023-02-19 10:25:44 -0500

    Uploading arduino part - it detects heart rate, but serial connection WIP

---
+++ arduino/sketch_feb18a/yeet.py
@@ -0,0 +1,16 @@
+import time
+import serial
+import serial.tools.list_ports 
+
+ports = serial.tools.list_ports.comports()
+for port, desc, hwid in sorted(ports):
+	print("{}: {} [{}]".format(port, desc, hwid))
+
+ser = serial.Serial("COM3", baudrate = 115200)
+print("COM3 connected")
+#ser.write(b'g')
+while 1:
+	serial_line = ser.readline()
+	#dumpfile.write(str(serial_line))
+	print(str(serial_line))
+
